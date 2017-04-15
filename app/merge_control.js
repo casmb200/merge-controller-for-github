9010bd5dbcd6e717c8a70f4433ede5afabed8c51
Ryo Nakajima <cas.mb200@gmail.com>
2017-04-15 16:47:03 +0900

    プルリク元の情報も判定に入れる
    プルリクエスト先のブランチだけでなく、プルリクエスト元のブランチでも判定できるようにする

--- app/merge_control.js
+++ app/merge_control.js
@@ -2,12 +2,6 @@ $(function() {
 
   chrome.storage.local.get(function(items) {
 
-    var BASE_URL = items.url;
-    var TARGET_USER = items.user;
-    var TARGET_REPOSITORY = items.repository;
-    var TARGET_BRANCH = items.branch;
-    var TITLE_RULE = items.title_rule;
-
     merge_control();
     $(window).on('statechange', function(e, data) {
       if (history.state.url !== undefined) {
@@ -18,7 +12,7 @@ $(function() {
     function merge_control() {
 
       // target Pull Request URL
-      if (location.href.search(BASE_URL) != -1 && location.href.search(/\/pull\//) != -1) {
+      if (location.href.search(items.github_url) != -1 && location.href.search(/\/pull\//) != -1) {
         $('.btn-group-merge').ready(function() {
           if (is_target_request() && is_target_title()) {
             $('.btn-group-merge>.js-details-target').attr('disabled', 'disabled');
@@ -32,20 +26,43 @@ $(function() {
     // target user, repository, and branch
     function is_target_request() {
       var base_ref = $('span.base-ref').attr('title') || '';
-      var array = base_ref.split(/[\/:]/);
-      if (array.length == 3) {
-        if (array[0] == TARGET_USER && array[1] == TARGET_REPOSITORY && array[2] == TARGET_BRANCH) {
+      var head_ref = $('span.head-ref').attr('title') || '';
+      if (base_ref.split(/[\/:]/).length >= 3 && head_ref.split(/[\/:]/).length >= 3) {
+        var base = {
+          "user": base_ref.slice(0, base_ref.indexOf("/")),
+          "repository": base_ref.slice(base_ref.indexOf("/") + 1, base_ref.indexOf(":")),
+          "branch": base_ref.slice(base_ref.indexOf(":") + 1)
+        }
+        var head = {
+          "user": head_ref.slice(0, head_ref.indexOf("/")),
+          "repository": head_ref.slice(head_ref.indexOf("/") + 1, head_ref.indexOf(":")),
+          "branch": head_ref.slice(head_ref.indexOf(":") + 1)
+        }
+        if (check(items.base_user, base.user) && check(items.base_repository, base.repository) && check(items.base_branch, base.branch)
+            && check(items.head_user, head.user) && check(items.head_repository, head.repository) && check(items.head_branch, head.branch)) {
           return true;
         }
       }
       return false;
     }
 
+    function check(storage, str) {
+      return (storage == '*' || storage == str);
+    }
+
     // target title
     function is_target_title() {
       var title = $('span.js-issue-title').html() || '';
-      if (title.search(/WIP/) != -1) {
-        return true;
+      var title_rules = [];
+      if (items.title_rule.search('|') == -1) {
+        title_rules.push(items.title_rules);
+      } else {
+        title_rules = items.title_rule.split('|');
+      }
+      for (var str of title_rules) {
+        if (title.search(str) != -1) {
+          return true;
+        }
       }
       return false;
     }
--- app/option.js
+++ app/option.js
@@ -8,27 +8,33 @@ $(function(){
       if (Object.keys(items).length === 0) {
         // Set Default
         var items = {}
-        items['url'] = 'https://github.com';
+        items['github_url'] = 'https://github.com';
         chrome.storage.local.set(items, function(e) {
           init();
         });
       }
-      $('#f_github_url').val(items.url);
+      $('#f_github_url').val(items.github_url);
       $('#f_title_rule').val(items.title_rule);
-      $('#f_user').val(items.user);
-      $('#f_repository').val(items.repository);
-      $('#f_branch').val(items.branch);
+      $('#f_base_user').val(items.base_user);
+      $('#f_base_repository').val(items.base_repository);
+      $('#f_base_branch').val(items.base_branch);
+      $('#f_head_user').val(items.head_user);
+      $('#f_head_repository').val(items.head_repository);
+      $('#f_head_branch').val(items.head_branch);
     });
   }
 
   // Save
   $('#b_save').on('click', function(e) {
     var items = {}
-    items['url'] = $('#f_github_url').val();
+    items['github_url'] = $('#f_github_url').val();
     items['title_rule'] = $('#f_title_rule').val();
-    items['user'] = $('#f_user').val();
-    items['repository'] = $('#f_repository').val();
-    items['branch'] = $('#f_branch').val();
+    items['base_user'] = $('#f_base_user').val();
+    items['base_repository'] = $('#f_base_repository').val();
+    items['base_branch'] = $('#f_base_branch').val();
+    items['head_user'] = $('#f_head_user').val();
+    items['head_repository'] = $('#f_head_repository').val();
+    items['head_branch'] = $('#f_head_branch').val();
     chrome.storage.local.set(items, function(e) {
       alert('Storage has been completed');
     });
