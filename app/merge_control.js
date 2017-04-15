087ac9bbb1026c58d3631b97815185c391eed87e
Ryo Nakajima <cas.mb200@gmail.com>
2017-04-15 20:48:33 +0900

    複数条件を設定可能にする
    ・複数の条件を設定できるようにする（上から優先）

--- app/merge_control.js
+++ app/merge_control.js
@@ -14,7 +14,7 @@ $(function() {
       // target Pull Request URL
       if (location.href.search(items.github_url) != -1 && location.href.search(/\/pull\//) != -1) {
         $('.btn-group-merge').ready(function() {
-          if (is_target_request() && is_target_title()) {
+          if (is_target_pull_request()) {
             $('.btn-group-merge>.js-details-target').attr('disabled', 'disabled');
             $('.btn-group-merge>.js-details-target').html('Do not merge!');
             $('.btn-group-merge>.js-menu-target').attr('disabled', 'disabled');
@@ -23,8 +23,7 @@ $(function() {
       }
     }
 
-    // target user, repository, and branch
-    function is_target_request() {
+    function is_target_pull_request() {
       var base_ref = $('span.base-ref').attr('title') || '';
       var head_ref = $('span.head-ref').attr('title') || '';
       if (base_ref.split(/[\/:]/).length >= 3 && head_ref.split(/[\/:]/).length >= 3) {
@@ -38,9 +37,16 @@ $(function() {
           "repository": head_ref.slice(head_ref.indexOf("/") + 1, head_ref.indexOf(":")),
           "branch": head_ref.slice(head_ref.indexOf(":") + 1)
         }
-        if (check(items.base_user, base.user) && check(items.base_repository, base.repository) && check(items.base_branch, base.branch)
-            && check(items.head_user, head.user) && check(items.head_repository, head.repository) && check(items.head_branch, head.branch)) {
-          return true;
+        for (var setting of items.settings) {
+          if (check(setting.base_user, base.user) && check(setting.base_repository, base.repository) && check(setting.base_branch, base.branch)
+            && check(setting.head_user, head.user) && check(setting.head_repository, head.repository) && check(setting.head_branch, head.branch)
+            && is_match_title_rule(setting.title_rule)) {
+              if (setting.merge == 'disallow') {
+                return true;
+              } else {
+                return false;
+              }
+          }
         }
       }
       return false;
@@ -50,17 +56,19 @@ $(function() {
       return (storage == '*' || storage == str);
     }
 
-    // target title
-    function is_target_title() {
-      var title = $('span.js-issue-title').html() || '';
-      var title_rules = [];
-      if (items.title_rule.search('|') == -1) {
-        title_rules.push(items.title_rules);
-      } else {
-        title_rules = items.title_rule.split('|');
+    function is_match_title_rule(title_rule) {
+      if (title_rule == '*') {
+        return true;
       }
-      for (var str of title_rules) {
-        if (title.search(str) != -1) {
+      var title = $('span.js-issue-title').html() || '';
+      var words = [];
+      if (title_rule.search('|') == -1) {
+        words.push(title_rule);
+      } else {
+        words = title_rule.split('|');
+      }
+      for (var word of words) {
+        if (title.search(word) != -1) {
           return true;
         }
       }
